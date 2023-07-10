8b0d977c78d551a8e7789596737ac839a0372463
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-07-10 19:45:46 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,8 @@
 function solution(n) {
-    var answer = '';
-    for(let i=1;i <= n;i++){
-        i % 2 == 0 ? answer += '박' : answer += '수'
-    }
-    return answer;
+    // var answer = '';
+    // for(let i=1;i <= n;i++){
+    //     i % 2 == 0 ? answer += '박' : answer += '수'
+    // }
+    // return answer;
+    return "수박".repeat(n).slice(0,n)
 }
