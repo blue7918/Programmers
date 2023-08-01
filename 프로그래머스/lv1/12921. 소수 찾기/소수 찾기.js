9a8d28572c73e89d5f556ba17c0229cabf864785
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-02 03:35:51 +0900

    [level 1] Title: 소수 찾기, Time: 146.68 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,17 +1,34 @@
-const isPrime = (n) => {
-    for(let i=3;i<=Math.sqrt(n);i+=2){
-        if(n%i == 0){
-            return false;
-        }
-    }
-    return true;    
-}
+// const isPrime = (n) => {
+//     for(let i=3;i<=Math.sqrt(n);i+=2){
+//         if(n%i == 0){
+//             return false;
+//         }
+//     }
+//     return true;    
+// }
+
+// function solution(n) {
+//     var answer = 1;
+//     if(n > 3) answer++;
+//     for(let i = 5 ; i <= n; i += 2){
+//         if(isPrime(i)) answer++;
+//     }
+//     return answer;
+// }
 
 function solution(n) {
-    var answer = 1;
-    if(n > 3) answer++;
-    for(let i = 5 ; i <= n; i += 2){
-        if(isPrime(i)) answer++;
+    const s = new Set();
+    for(let i=1; i<=n; i+=2){
+        s.add(i);
     }
-    return answer;
+    s.delete(1);
+    s.add(2);
+    for(let j=3; j<Math.sqrt(n); j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
+        }
+    }
+    return s.size;
 }
