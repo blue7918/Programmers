86f8e32fc04fedb51358ee7c3f8523be77396d37
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-01 15:06:31 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,12 +1,24 @@
 function solution(cards1, cards2, goal) {
-    for(let s of goal) {
-            if(cards1[0] === s) {
-                cards1.shift();
-            } else if(cards2[0] === s) {
-                cards2.shift();
-            } else {
-                return "No"
-            }
+    // for(let s of goal) {
+    //         if(cards1[0] === s) {
+    //             cards1.shift();
+    //         } else if(cards2[0] === s) {
+    //             cards2.shift();
+    //         } else {
+    //             return "No"
+    //         }
+    //     }
+    // return "Yes";
+    
+    let idx1 = 0;
+    let idx2 = 0;
+    for(let i = 0 ; i < goal.length ; i++ ){
+        if(goal[i] == cards1[idx1]) idx1++;
+        else if(goal[i] == cards2[idx2]) idx2++;
+        else{ 
+            return "No" 
+            break;
         }
-    return "Yes";
+    }
+    return "Yes"
 }
