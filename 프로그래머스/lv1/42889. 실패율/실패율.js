b89d0c96296325ee3f2103a9b404fbe8f40322ff
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-03 17:46:15 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,23 +1,12 @@
-// function solution(N, stages) {
-//     var answer = [];
-    
-//     for(let i = 1 ; i <= N ; i++){
-//         let reach = stages.filter((stage)=> stage >= i).length;
-//         let notYet = stages.filter((stage)=> stage === i).length;
-//         answer.push({index: i, per:notYet/reach});
-//     }
-//     answer.sort((a,b)=> b.per - a.per);
-//     return answer.map((item)=>item.index);
-    
-// }
-
 function solution(N, stages) {
-    let result = [];
-    for(let i=1; i<=N; i++){
-        let reach = stages.filter((x) => x >= i).length;
-        let curr = stages.filter((x) => x === i).length;
-        result.push([i, curr/reach]);
+    var answer = [];
+    
+    for(let i = 1 ; i <= N ; i++){
+        let reach = stages.filter((stage)=> stage >= i).length;
+        let notYet = stages.filter((stage)=> stage === i).length;
+        answer.push({index: i, per:notYet/reach});
     }
-    result.sort((a,b) => b[1] - a[1]);
-    return result.map((x) => x[0]);
+    answer.sort((a,b)=> b.per - a.per);
+    return answer.map((item)=>item.index);
+    
 }
