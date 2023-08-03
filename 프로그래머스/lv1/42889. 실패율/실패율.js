d2cdc500c9eecfe4b420d4b28c278352d3102d7e
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-03 17:43:28 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,21 +1,10 @@
 function solution(N, stages) {
     var answer = [];
-    let reach = [];
-    let notYet = [];
-    (reach = []).length = N+1;
-    (notYet = []).length = N+1;
-    reach.fill(0);
-    notYet.fill(0);
     
-    stages.map((item)=>{
-        if(item <= N) notYet[item]++;
-        if(item > N) item = N;
-        for(let i = 1 ; i <= item ; i++){
-            reach[i]++;
-        }
-    })
-    for(let i = 0 ; i < N ; i++){
-        answer.push({index: i+1, per:notYet[i+1]/reach[i+1]});
+    for(let i = 1 ; i <= N ; i++){
+        let reach = stages.filter((stage)=> stage >= i).length;
+        let notYet = stages.filter((stage)=> stage === i).length;
+        answer.push({index: i, per:notYet/reach});
     }
     answer.sort((a,b)=> b.per - a.per);
     return answer.map((item)=>item.index);
