0b8d660bcb436644c353ba71fa81655f2f64f0a5
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-03 22:41:32 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.32 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,16 +1,18 @@
+const getDivisors = (num) => {
+    let divisors = 0;
+    for(let i = 1 ; i <= Math.sqrt(num) ; i++){
+        if(num % i === 0) {
+            divisors++;
+            if(num / i != i) divisors++;
+        }
+    }
+    return divisors;
+}
+
 function solution(number, limit, power) {
     var answer = 0;
-    const getDivisors = (num) => {
-        let divisors = 0;
-        for(let i = 1 ; i <= Math.sqrt(num) ; i++){
-            if(num % i === 0) {
-                divisors++;
-                if(num / i != i) divisors++;
-            }
-        }
-        return divisors;
-    }
     for(let i = 1 ; i <= number; i++){
+        //getDivisors(i) > limit ? answer += power : answer += getDivisors(i); 이게 조금 더 느린 것 같아서 아래와 같이 변경
         let temp = getDivisors(i);
         temp > limit ? answer += power : answer += temp;
     }
