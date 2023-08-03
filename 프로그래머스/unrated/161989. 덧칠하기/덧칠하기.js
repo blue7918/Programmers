8480e92c504d9d124717aefffad3701d3e72cebf
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-03 22:21:33 +0900

    [unrated] Title: 덧칠하기, Time: 1.70 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,7 +1,7 @@
 function solution(n, m, section) {
     var answer = 1;
     let last = section[0] + m - 1;
-    for(let i = 0 ; i < section.length; i++){
+    for(let i = 1 ; i < section.length; i++){
         if(section[i] > last) {
             last = section[i] + m - 1;
             answer++;
