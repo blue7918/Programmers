f5368d046dce6b829c56cfc671e39274df0e3e46
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-04 17:09:27 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,20 +1,22 @@
-// function solution(lottos, win_nums) {
-//     let answer = [1,1];
-//     lottos.forEach((item)=>{
-//         if(item == 0) answer[1] += 1;
-//         else if(!win_nums.includes(item)){
-//             answer[0] += 1;   
-//             answer[1] += 1;
-//         }
-//     })
-//     answer[1] > 6 ? answer[1] = 6 : '';
-//     return answer;
-// }
-
 function solution(lottos, win_nums) {
-  const rank = [6, 6, 5, 4, 3, 2, 1];
-  let minCount = lottos.filter((v) => win_nums.includes(v)).length;
-  let zeroCount = lottos.filter((v) => !v).length;
-  let maxCount = minCount + zeroCount;
-  return [rank[maxCount], rank[minCount]];
+    let answer = [1,1];
+    lottos.forEach((item)=>{
+        if(item == 0) answer[1] += 1;
+        else if(!win_nums.includes(item)){
+            answer[0] += 1;   
+            answer[1] += 1;
+        }
+    })
+    
+    answer[0] > 6 ? answer[0] = 6 : '';
+    answer[1] > 6 ? answer[1] = 6 : '';
+    return answer;
 }
+
+// function solution(lottos, win_nums) {
+//   const rank = [6, 6, 5, 4, 3, 2, 1];
+//   let minCount = lottos.filter((v) => win_nums.includes(v)).length;
+//   let zeroCount = lottos.filter((v) => !v).length;
+//   let maxCount = minCount + zeroCount;
+//   return [rank[maxCount], rank[minCount]];
+// }
