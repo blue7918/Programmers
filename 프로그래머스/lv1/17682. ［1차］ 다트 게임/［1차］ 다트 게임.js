026910172d546a2303f1cdd7baec2c27f4d4fa4b
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-04 16:23:13 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,24 @@
+ function solution(dartResult) {
+    var answer = [0,0,0];
+    let answerIndex = 0;
+    let bonus = {"S":1, "D":2, "T":3};
+    
+    for(let i = 0; i < dartResult.length; i++){
+        var char = dartResult[i];
+        if(/[0-9]/.test(char)){
+            if(dartResult[i]==='1' && dartResult[i+1]==='0') {
+                answer[answerIndex] += 10;
+                i++;
+            } else answer[answerIndex] +=dartResult[i];
+        }else if(/[A-Z]/.test(char)){
+            answer[answerIndex] = Math.pow(answer[answerIndex], bonus[char]);
+            answerIndex++;
+        }else if(char ==='*') {
+            if(answerIndex > 1){
+                answer[answerIndex -1] *= 2;
+                answer[answerIndex - 2] *= 2;    
+            }else answer[answerIndex -1] *= 2;
+        } else if( char === '#') answer[answerIndex -1] *= (-1);
+    }
+    return answer.reduce((acc, val) => {return acc + val});
+}
