5b46da27290b2125070cbb484a2d806a376b6216
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-08 15:42:49 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,24 +1,46 @@
+// function solution(n, lost, reserve) {
+//     let student = n - lost.length;
+//     let realLost = lost.filter(i => !reserve.includes(i));
+//     let realReserve = reserve.filter(i => !lost.includes(i));
+//     realLost = realLost.sort((a,b) => a-b);
+//     realReserve = realReserve.sort((a,b) => a-b);
+//      console.log(realLost)
+//     console.log(realReserve)
+//     for(let j = 0 ; j < realReserve.length ; j++) {
+//         for(let i = 0 ; i < realLost.length ; i++){
+//             if( Math.abs(realLost[i] - realReserve[j]) <= 1){
+//                 delete(realReserve[j]);
+//                 student++;
+//             }    
+//         }   
+//     }
+//     return student >= n ? n : student ;
+// }
+
 function solution(n, lost, reserve) {
-    let student = n -lost.length;
-    lost = lost.sort((a,b)=>a-b);
-    reserve = reserve.sort((a,b)=>a-b);
-    for(let j = 0 ; j < reserve.length ; j++) {
-        for(let i = 0 ; i < lost.length ; i++){
-            if(lost[i] ==  reserve[j]){
-                delete(lost[i]);
-                delete(reserve[j]);
-                student++;
-            }    
-        }   
-    }
-    for(let j = 0 ; j < reserve.length ; j++) {
-        for(let i = 0 ; i < lost.length ; i++){
-            if(lost[i] == reserve[j] - 1 || lost[i] ==  reserve[j] + 1 ){
-                delete(lost[i]);
-                delete(reserve[j]);
-                student++;
-            }    
-        }   
-    }
-    return student >= n ? n : student;
+    
+    let answer = 0;
+    
+    // 체육복을 도난당하고 여벌이 없는 학생(여벌 받아야하는 학생)
+    const noReserveLost = lost.sort((a,b)=>a-b).filter((lost) => !reserve.includes(lost));
+    
+    // 여벌이 있고 체육복을 도난당하지 않은 학생(빌려줄 수 있는 학생)
+    let hasReserve = reserve.sort((a,b)=>a-b).filter((reverse) => !lost.includes(reverse));
+    
+    const finalLost = noReserveLost.filter((lost) => {
+        
+        // 첫번째로 체육복을 빌려줄 수 있는 사람 
+        const lend = hasReserve.find((reserve) => Math.abs(reserve - lost) == 1);
+        
+        // 체육복 빌려줄 사람이 없으면 그대로 lost 리턴
+        if(!lend) return lost;
+        
+        // 빌려준 사람 제외하기
+        hasReserve = hasReserve.filter((reverse) => reverse !== lend);
+    })
+  
+    // 답 = 전체 학생 수 - 체육복이 없는 학생 수 
+    answer = n - finalLost.length;
+    
+    return answer;
 }
