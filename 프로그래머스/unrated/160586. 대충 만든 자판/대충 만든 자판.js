6f1c178a0e9e3e67b650be4675b01630542659ee
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-08 18:10:33 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,24 +1,36 @@
+// function solution(keymap, targets) {
+//     var answer = [];
+//     let keyArray = [];
+//     keymap.map((key)=>{
+//         for(let i = 0 ; i < key.length ; i++){
+//             let index = key[i].charCodeAt()-65;
+//             keyArray[index]? keyArray[index] = Math.min(keyArray[index], i+1) : keyArray[index] = i+1;
+//         }
+//     })
+//     console.log(keyArray)
+//     targets.map((word)=>{
+//         let sum = 0;
+//         for(let i = 0 ; i < word.length ; i++){
+//             let value = keyArray[word[i].charCodeAt()-65]
+//             if(value) sum += value;
+//             else{
+//                 sum = -1;
+//                 break;
+//             }
+//         }
+//         answer.push(sum);
+//     })
+//     return answer;
+// }
+
 function solution(keymap, targets) {
-    var answer = [];
-    let keyArray = [];
-    keymap.map((key)=>{
-        for(let i = 0 ; i < key.length ; i++){
-            let index = key[i].charCodeAt()-65;
-            keyArray[index]? keyArray[index] = Math.min(keyArray[index], i+1) : keyArray[index] = i+1;
-        }
-    })
-    console.log(keyArray)
-    targets.map((word)=>{
-        let sum = 0;
-        for(let i = 0 ; i < word.length ; i++){
-            let value = keyArray[word[i].charCodeAt()-65]
-            if(value) sum += value;
-            else{
-                sum = -1;
-                break;
-            }
-        }
-        answer.push(sum);
-    })
+    const answer = [];
+    const map = {}
+    for (const items of keymap) {
+        items.split('').map((item, index) => map[item] = (map[item] < index+1 ? map[item] : index+1))
+    }
+    for (const items of targets) {
+        answer.push(items.split('').reduce((cur, item) => cur += map[item], 0) || -1)
+    }
     return answer;
 }
