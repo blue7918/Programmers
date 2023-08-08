eb5b3a3dd5f1152d42ee6eaff551faa088cdd6e7
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-08 16:27:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.64 ms, Memory: 57.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+    completion = completion.sort();
+    participant = participant.sort();
+    for(let i =0; i < participant.length ; i++){
+        if(completion[i] != participant[i]) return participant[i];
+    }
+    // participant.map((item, index)=>{
+    //     if(item != completion[index]) console.log(item);
+    // });
+}
