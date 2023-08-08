23e32ab615ac0d10109f3b9d04864e2324fb85f7
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-08 17:28:38 +0900

    [unrated] Title: 문자열 나누기, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -1,16 +1,14 @@
 function solution(s) {
     let result = 0 ;
-    let index = 0 ;
-    let x = "";
+    let x = s[0];
     let countX = 0;
     let countOther = 0;
     
     for(let i = 0 ; i < s.length ; i++){
-        x = s[index];
         if(s[i] === x) ++countX;
         else ++countOther;
         if(countX != 0 && countX === countOther){
-            index = i+1; countX = 0; countOther = 0; ++result; x = s[index];
+            countX = 0; countOther = 0; ++result; x = s[i+1];
         }
         
     }
