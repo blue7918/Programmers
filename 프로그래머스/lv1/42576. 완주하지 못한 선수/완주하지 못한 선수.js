15548db244878517cd07b5a233eac56e8d8d0cec
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-08 16:29:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.99 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,11 @@
-function solution(participant, completion) {
-    completion = completion.sort();
-    participant = participant.sort();
-    for(let i =0; i < participant.length ; i++){
-        if(completion[i] != participant[i]) return participant[i];
-    }
-    // participant.map((item, index)=>{
-    //     if(item != completion[index]) console.log(item);
-    // });
-}
+// function solution(participant, completion) {
+//     completion = completion.sort();
+//     participant = participant.sort();
+//     for(let i =0; i < participant.length ; i++){
+//         if(completion[i] != participant[i]) return participant[i];
+//     }
+//     // participant.map((item, index)=>{
+//     //     if(item != completion[index]) console.log(item);
+//     // });
+// }
+var solution=(participant,completion)=>participant.find(name=>!completion[name]--,completion.map(name=>completion[name]=(completion[name]|0)+1))
