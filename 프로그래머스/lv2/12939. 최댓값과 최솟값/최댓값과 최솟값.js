4582f113db346e5f7843bbbccc9b2995f8a07fa3
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-18 06:51:10 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,5 @@
 function solution(s) {
     let arr = s.split(/\s+/); 
     arr.sort((a,b)=>a-b);
-    return (arr[0].concat(" ", arr[arr.length -1]));
+    return (Math.min(...arr)+" "+Math.max(...arr));
 }
