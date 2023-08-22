f5c864abedcf6b923488bea322d6d06f4ae83558
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-22 18:57:22 +0900

    [level 2] Title: 피보나치 수, Time: 4.65 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,3 @@
-// function solution(n) {
-//     let sqrt5 = Math.sqrt(5);
-//     let result =  (1/sqrt5) * (Math.pow((1+sqrt5)/2, n) - Math.pow((1-sqrt5)/2, n));
-//     return parseInt(result%1234567);
-// }
-
 function solution(n) {
     let fib = [0, 1];
     for(let i=2; i<=n; i++) {
