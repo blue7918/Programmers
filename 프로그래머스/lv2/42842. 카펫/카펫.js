bf33541b519b830e57380069760a101bc09c84d8
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-26 22:02:42 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,7 +1,16 @@
+// function solution(brown, yellow) {
+//     for(let i =1; i <= brown; i++){
+//         for(let j = 1 ; j <= i; j++){
+//             if(i*j===yellow && (i+2)*2+(j)*2 === brown) return [i+2,j+2]
+//         }
+//     }
+// }
 function solution(brown, yellow) {
-    for(let i =1; i <= brown; i++){
-        for(let j = 1 ; j <= i; j++){
-            if(i*j===yellow && (i+2)*2+(j)*2 === brown) return [i+2,j+2]
-        }
+    let total = brown + yellow;
+
+    for (let i = 3; i <= Math.sqrt(total); i++) {
+        if (total % i != 0) continue;
+        if ((i - 2) * (total / i - 2) == yellow) return [total / i, i];
     }
+    return null;
 }
