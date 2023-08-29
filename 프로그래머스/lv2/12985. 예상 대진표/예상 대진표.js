d12cd3d441c7ca63ee7d7081e8c0e4ca642ce7f8
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-29 21:50:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -9,15 +9,12 @@
 //         }
 //     }
 // }
-// function solution(n,a,b){
-//     let answer = 0;
-//     while(a !== b) {
-//         a = Math.ceil(a/2);
-//         b = Math.ceil(b/2);
-//         answer++;
-//     }
-//     return answer;
-// }
-function solution( n,  a,  b){
-    return ((a-1)^(b-1)).toString(2).length;
+function solution(n,a,b){
+    let answer = 0;
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        answer++;
+    }
+    return answer;
 }
