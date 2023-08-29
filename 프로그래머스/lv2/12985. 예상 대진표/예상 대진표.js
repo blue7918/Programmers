ea6041594d8a2411c2d73a61b57719106cfe4f8b
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-08-29 21:49:44 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,17 +1,23 @@
-function solution(n,a,b){
-    let answer = 0;
-    let teamA = Math.ceil(a/2);
-    let teamB = Math.ceil(b/2);
-    for(let i =1; i<=n/2 ; i++){
-        console.log(teamA)
-        console.log(teamB)
-        answer++;
-        if(teamA === teamB){
-            return answer;
-            break;
-        }
-        teamA = Math.ceil(teamA/2);
-        teamB = Math.ceil(teamB/2);
-    }
-    return answer;
+// function solution(n,a,b){
+//     let answer = 0;
+//     for(let i =1; i<=n/2 ; i++){
+//         a = Math.ceil(a/2);
+//         b = Math.ceil(b/2);
+//         answer++;
+//         if(a === b){
+//             return answer;
+//         }
+//     }
+// }
+// function solution(n,a,b){
+//     let answer = 0;
+//     while(a !== b) {
+//         a = Math.ceil(a/2);
+//         b = Math.ceil(b/2);
+//         answer++;
+//     }
+//     return answer;
+// }
+function solution( n,  a,  b){
+    return ((a-1)^(b-1)).toString(2).length;
 }
