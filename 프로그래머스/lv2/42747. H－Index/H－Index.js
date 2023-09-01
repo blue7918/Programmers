bcba960d7b3175136cfc4febcff8e997dfa7c83e
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-01 20:20:32 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,8 +1,11 @@
-function solution(citations) {
-    let answer = 0;
-    let cit =  citations.sort((a, b) => b - a);
-    for(let i = 1 ; i <= citations.length ; i++){
-        if(i<=citations[i-1]) answer++;
-    }
-    return answer;
-}
+// function solution(citations) {
+//     let answer = 0;
+//     let cit =  citations.sort((a, b) => b - a);
+//     for(let i = 1 ; i <= cit.length ; i++){
+//         if(i<=cit[i-1]) answer++;
+//     }
+//     return answer;
+// }
+
+
+const solution = (citations) => citations.sort((a, b) => b - a).filter((el, idx) => el >= idx + 1).length;
