7d976809fedd5193a655e9d3696ab9d4ed4788e3
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-05 19:39:34 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,19 +1,14 @@
 function solution(progresses, speeds) {
-    let answer = [];
-    let time = [];
-    for(let i = 0; i < speeds.length; i++){
-        time.push( Math.ceil((100 - progresses[i]) / speeds[i]) )
-    }
-    let count = 0;
+    let answer = [0];
+    let time = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    
     let index = time[0];
-    for(let i = 0; i < speeds.length; i++){
-        if(index >= time[i]) count++
+    for(let i = 0, j = 0; i < speeds.length; i++){
+        if(index >= time[i]) ++answer[j];
         else{
             index = time[i];
-            answer.push(count);
-            count = 1;
+            answer[++j] = 1;
         }
     }
-    answer.push(count);
     return answer;
 }
