e4cb6fa5dabb1bcc02503c737c673915c83d69f1
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-15 19:55:28 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,43 +1,62 @@
-function solution(fees, records) {
-  const cars = {};
+// function solution(fees, records) {
+//   const cars = {};
     
-  records.forEach(v => {
-    // 정보 쪼개기
-    let [time, car, type] = v.split(" ");
+//   records.forEach(v => {
+//     // 정보 쪼개기
+//     let [time, car, type] = v.split(" ");
         
-    const [hour, minute] = time.split(":");
+//     const [hour, minute] = time.split(":");
         
-    // 분으로 바꾸기
-    time = hour * 60 + Number(minute);
+//     // 분으로 바꾸기
+//     time = hour * 60 + Number(minute);
     
-    // 처음 조회되는 차량일 시
-    if (!cars[car]) {
-      cars[car] = { time: 0, car };
-    }
+//     // 처음 조회되는 차량일 시
+//     if (!cars[car]) {
+//       cars[car] = { time: 0, car };
+//     }
         
-    cars[car].type = type;
+//     cars[car].type = type;
         
-    if (type == "OUT") {
-        cars[car].time += time - cars[car].lastInTime;
-        return;
-    }
+//     if (type == "OUT") {
+//         cars[car].time += time - cars[car].lastInTime;
+//         return;
+//     }
         
-    cars[car].lastInTime = time;
-  });
+//     cars[car].lastInTime = time;
+//   });
 
-  return Object.values(cars)
-    .sort((a, b) => a.car - b.car)
-    .map(v => {
-      // 차량이 최종적으로 나가지 않았을 때
-      if (v.type == "IN") {
-        v.time += 1439 - v.lastInTime;
-      }
+//   return Object.values(cars)
+//     .sort((a, b) => a.car - b.car)
+//     .map(v => {
+//       // 차량이 최종적으로 나가지 않았을 때
+//       if (v.type == "IN") {
+//         v.time += 1439 - v.lastInTime;
+//       }
       
-      // 기본시간을 넘지 않았을 때
-      if (fees[0] > v.time) {
-        return fees[1];
-      }
+//       // 기본시간을 넘지 않았을 때
+//       if (fees[0] > v.time) {
+//         return fees[1];
+//       }
             
-      return fees[1] + Math.ceil((v.time - fees[0]) / fees[2]) * fees[3];
+//       return fees[1] + Math.ceil((v.time - fees[0]) / fees[2]) * fees[3];
+//     });
+// }
+
+function solution(fees, records) {
+    const parkingTime = {};
+    records.forEach(r => {
+        let [time, id, type] = r.split(' ');
+        let [h, m] = time.split(':');
+        time = (h * 1) * 60 + (m * 1);
+        if (!parkingTime[id]) parkingTime[id] = 0;
+        if (type === 'IN') parkingTime[id] += (1439 - time);
+        if (type === 'OUT') parkingTime[id] -= (1439 - time);
     });
+    const answer = [];
+    for (let [car, time] of Object.entries(parkingTime)) {
+        if (time <= fees[0]) time = fees[1];
+        else time = Math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1]
+        answer.push([car, time]);
+    }
+    return answer.sort((a, b) => a[0] - b[0]).map(v => v[1]);
 }
