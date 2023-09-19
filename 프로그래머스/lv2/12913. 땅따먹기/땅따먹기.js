174b1412b6e1880d2369eda69fced4c6ac62536f
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-19 19:18:57 +0900

    [level 2] Title: 땅따먹기, Time: 30.20 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,18 +1,12 @@
 function solution(land) {
-    let answer = 0;
-    
-    for(let i = 0; i < land.length; i++){
-       for(let j = 0; j < 4; j++){
-           if(i === 0){
-               continue;
-           } else {
-               let arr = land[i-1].slice();
-               arr[j] = 0;
-               land[i][j] += Math.max.apply(null, arr);
-               answer = Math.max(land[i][j], answer);
-           }
-       }       
-    }
-    
-    return answer;
+    var answer = 0;
+
+    return Math.max(...land.reduce((a, c) => {
+        return [
+            c[0] + Math.max(a[1], a[2], a[3]),  
+            c[1] + Math.max(a[0], a[2], a[3]),
+            c[2] + Math.max(a[0], a[1], a[3]),
+            c[3] + Math.max(a[0], a[1], a[2]),
+        ];
+    }, [0, 0, 0, 0]));
 }
