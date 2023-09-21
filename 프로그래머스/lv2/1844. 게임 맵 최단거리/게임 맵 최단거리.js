9031516c945bfd04d0badc4246487c5d8e0ae4f8
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-22 01:16:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 23.37 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,36 +1,63 @@
 function solution(maps) {
-    let answer = 1;
-    let visited = maps;
-    let queue = [];
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
+    // 1. n 과 m 을 설정함
     const n = maps.length;
     const m = maps[0].length;
-    
-    queue.push([0, 0]);
-    visited[0][0] = 0;
-    
-    while(queue.length > 0) {
-        let size = queue.length;
-        
-        for(let i = 0; i < size; i++) {
-            let [x, y] = queue.shift();
-            
-            for(let j = 0; j < 4; j++) {
-                let nx = x + dx[j];
-                let ny = y + dy[j];
-                
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && visited[nx][ny] === 1) {
-                    if(nx == n - 1 && ny == m - 1) {
-                        return ++answer;
-                    }
-                    queue.push([nx, ny]);
-                    visited[nx][ny] = 0;
-                }
+
+    // 2. 정답 저장용
+    let answer = -1;
+
+    // 3. 방문을 체크할 배열
+    let visited = Array.from(Array(n), () => Array(m).fill(false));
+
+    // 4. BFS 를 하기 위한 queue, 초기 값을 저장해둠
+    let queue = [[0, 0, 1]];
+
+    // 5. queue 의 위치를 저장할 queueIndex
+    let queueIndex = 0;
+
+    // 6. x, y 가 움직일 배열을 저장함 (상, 우, 하, 좌)
+    const moveX = [-1, 0, 1, 0];
+    const moveY = [0, 1, 0, -1];
+
+    // 7. BFS 진행할 while 문
+    while (queue.length > queueIndex) {
+        // 8. 일단 queue 에 있는 값을 꺼냄
+        const now = queue[queueIndex];
+        // 9. 값을 꺼냈으므로 index 를 +1 해줌
+        queueIndex += 1;
+
+        // 10. 만약 꺼낸 값이 정답 (도착지) 이면
+        if (now[0] == n - 1 && now[1] == m - 1) {
+            // 11. answer 에 답을 저장함 (now[2] 는 이동 거리)
+            answer = now[2];
+            break;
+        }
+
+        // 12. 만약 꺼낸 값이 방문하지 않은 값이라면
+        if (!visited[now[0]][now[1]]) {
+            // 13. 방문
+            visit(now[0], now[1], now[2])
+        }
+    }
+
+    // 14. 방문 함수. x, y 좌표와 count (이동거리) 를 파라미터로 받음
+    function visit(x, y, count) {
+        // 15. 먼저 방문했다고 체크함
+        visited[x][y] = true;
+
+        // 16. 현재 x, y 위치에서 상, 하, 좌, 우 로 이동할 반복문
+        for (let i = 0; i < moveX.length; i++) {
+            // 17. movedX, Y 로 설정함.
+            const movedX = x + moveX[i];
+            const movedY = y + moveY[i];
+
+            // 18. 만약, movedX, movedY 가 배열의 범위 안에 있고, 그 값 위치가 아직 방문하지 않았고, 그 위치를 방문할 수 있다면 (값이 1이라면)
+            if (movedX >= 0 && movedX < n && movedY >= 0 && movedY < m && !visited[movedX][movedY] && maps[movedX][movedY] == 1) {
+                // queue 에 그 값을 넣음
+                queue.push([x + moveX[i], y + moveY[i], count + 1]);
             }
         }
-        answer++;
     }
-    
-    return -1;
+
+    return answer;
 }
