20af5a0e16564e952409978ba403d4620e546779
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-21 17:23:06 +0900

    [level 2] Title: 스킬트리, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,13 +1,18 @@
-function solution(skill, skill_trees) {
-    function isCorrect(n) {
-        let test = skill.split('');
-        for (var i = 0; i < n.length; i++) {
-            if (!skill.includes(n[i])) continue;
-            if (n[i] === test.shift()) continue;
-            return false;
-        }
-        return true;
-    }    
+// function solution(skill, skill_trees) {
+//     function isCorrect(n) {
+//         let test = skill.split('');
+//         for (var i = 0; i < n.length; i++) {
+//             if (!skill.includes(n[i])) continue;
+//             if (n[i] === test.shift()) continue;
+//             return false;
+//         }
+//         return true;
+//     }    
 
-    return skill_trees.filter(isCorrect).length;
+//     return skill_trees.filter(isCorrect).length;
+// }
+
+function solution(skill, skill_trees) {
+    const skills = skill.split('');
+    return skill_trees.filter(tree => skill.indexOf(tree.split('').filter(s => skills.includes(s)).join('')) === 0).length;
 }
