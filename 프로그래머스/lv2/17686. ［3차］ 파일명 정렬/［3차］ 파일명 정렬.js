6cfadf31916fd28fc4da17e0c88042bb4036011d
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-09-26 19:14:18 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.02 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,14 +1,15 @@
 function solution(files) {
-  return files.sort((a, b) => {
-    const aHead = a.match(/^\D+/)[0].toLowerCase();
-    const bHead = b.match(/^\D+/)[0].toLowerCase();
+    let dict = []
+    files.forEach((entry, idx) => {
+        let [fn, head, num] = entry.match(/^([a-zA-Z-\. ]+)([0-9]+)(.*)$/)
+        dict.push({fn, head: head.toLowerCase(), num: parseInt(num), idx})
+    })
 
-    if(aHead < bHead) return -1;
-    if(aHead > bHead) return 1;
-
-    const aNum = a.match(/\d+/)[0].replace(/^0+/, '');
-    const bNum = b.match(/\d+/)[0].replace(/^0+/, '');
-
-    return aNum - bNum;
-  });
+    return dict.sort((a, b) => {
+        if (a.head > b.head) return  1
+        if (a.head < b.head) return -1
+        if (a.num > b.num) return  1
+        if (a.num < b.num) return -1
+        return a.idx - b.idx
+    }).map(e => e.fn)
 }
