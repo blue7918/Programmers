c5e6a1bc57cbc2570047c804d4c151f3cd5dd116
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-10-05 12:56:07 +0900

    [unrated] Title: 롤케이크 자르기, Time: 73.11 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,35 +1,27 @@
-// function solution(topping) {
-//   let answer = 0;
-
-//   const allTopping = new Map();
-//   const bro = new Map();
-
-//   topping.forEach((n) => {
-//     allTopping.set(n, (allTopping.get(n) || 0) + 1);
-//   });
-
-//   for (let n of topping) {
-//     allTopping.set(n, allTopping.get(n) - 1);
-//     bro.set(n, true);
-//     if (!allTopping.get(n)) allTopping.delete(n);
-//     if (allTopping.size === bro.size) answer++;
-//     if (allTopping.size < bro.size) break;
-//   }
-
-//   return answer;
-// }
-
 function solution(topping) {
-  const [b1Dict, b1Arr, b2Dict, b2Arr] = [new Set(), [], new Set(), []];
-  topping.forEach((t1, i) => {
-    const t2 = topping.at(-i);
-    b1Dict.add(t1);
-    if (i !== 0) b2Dict.add(t2);
-    b1Arr.push(b1Dict.size);
-    b2Arr.push(b2Dict.size);
-  });
-  return b1Arr.reduce(
-    (acc, cur, i) => (cur === b2Arr.at(-i - 1) ? acc + 1 : acc),
-    0
-  );
+    const a = new Set()
+    const b = {}
+
+    let answer = 0;
+    let check = 0
+
+    for (let i = 0; i < topping.length; i++) {        
+        if (b[topping[i]]) {
+            b[topping[i]]++
+        } else {
+            b[topping[i]] = 1
+            check++            
+        }
+    }
+
+    for (let i = 0; i < topping.length; i++) {
+        a.add(topping[i])
+        b[topping[i]]--
+
+        if (!b[topping[i]]) check--
+        if (a.size === check) answer++
+    }
+
+
+    return answer;
 }
