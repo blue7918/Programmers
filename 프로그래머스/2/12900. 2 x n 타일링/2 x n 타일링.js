bc2f164edce4f21b4530e2aeb19b6f7b91690c2f
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-10-10 19:19:13 +0900

    [level 2] Title: 2 x n 타일링, Time: 4.26 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,10 +1,8 @@
 function solution(n) {
-    let answer = 0;
-    const dp = Array(n).fill(0)
-    dp[1] = 1
-    dp[2] = 2
-    for(var i = 3; i <= n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
-    }
-    return dp[n];
+  const arr = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000007;
+  }
+
+  return arr[n];
 }
