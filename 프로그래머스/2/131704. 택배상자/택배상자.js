0d48c0c12c53cf88f47e257633d6eec2633eebb7
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-10-13 21:00:26 +0900

    [level 2] Title: 택배상자, Time: 77.60 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,20 +1,17 @@
 function solution(order) {
-    let answer = 0;
     let stack = [];
     let idx = 0;
     for(let i = 1; i <= order.length; i++){
         if(order[idx] !== i) stack.push(i);
         else{
             idx++;
-            answer++
         }
         
         while(stack.length !== 0 && stack.at(-1) === order[idx]){
             stack.pop();
             idx++;
-            answer++;
         }
    
     }
-    return answer;
+    return idx;
 }
