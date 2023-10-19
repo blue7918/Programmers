070a67e9f80cc484df7e622125302c43a656ce6e
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-10-19 19:35:58 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 17.59 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,35 +1,6 @@
-// function solution(arr) {
-//     const answer = [0, 0];
-//     const m = arr.length;
-    
-//     const check = (sx, sy, length) => {
-//         const first = arr[sx][sy];
-//         const half = Math.floor(length / 2);
-        
-//         for(let i = sx; i < sx + length; i++){
-//             for(let j = sy; j < sy + length; j++){
-//                 if(arr[i][j] !== first){
-//                     check(sx, sy, half);
-//                     check(sx + half, sy, half);
-//                     check(sx, sy + half, half);
-//                     check(sx + half, sy + half, half);
-                    
-//                     return;
-//                 }
-//             }
-//         }
-        
-//         answer[first]++;
-//     }
-    
-//     check(0, 0, m);
-    
-//     return answer;
-// }
-
 function solution(arr) {
     const quadZip = (row, col, n) => {
-        if(n < 2) return arr[row][col] ? [0, 1] : [1, 0];
+        if(n < 2) return arr[row][col] ? [0, 1] : [1, 0]; //한칸일 경우 각 0,1 count
         let cnt0 = 0, cnt1 = 0; n >>= 1;
         for(let [i, j] of [[0,0],[0,1],[1,0],[1,1]]) {
             let [zero, one] = quadZip(row+i*n, col+j*n, n);
