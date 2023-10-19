a50113569d00ac334ad2da12a90fee67a644e493
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-10-19 20:47:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,16 +1,36 @@
-function solution(sequence, k) {
-    let [l, r] = [0, 0]
-    let summed = sequence[l] // 현재 쌓여있는 합 (l~r위치 값의 합)
-    const candidate = [] // 답안 후보
-    while (r < sequence.length) {
-        if (summed < k) summed += sequence[++r] // 합이 k보다 작으면 r이동
-        else if (summed > k) summed -= sequence[l++] // 합이 k보다 크면 l이동
-        else { // 합이 k라면 후보에 넣고 r과 l이동
-            candidate.push([l, r])
-            summed += sequence[++r]
-            summed -= sequence[l++]
+// function solution(sequence, k) {
+//     let [l, r] = [0, 0]
+//     let summed = sequence[l] // 현재 쌓여있는 합 (l~r위치 값의 합)
+//     const candidate = [] // 답안 후보
+//     while (r < sequence.length) {
+//         if (summed < k) summed += sequence[++r] // 합이 k보다 작으면 r이동
+//         else if (summed > k) summed -= sequence[l++] // 합이 k보다 크면 l이동
+//         else { // 합이 k라면 후보에 넣고 r과 l이동
+//             candidate.push([l, r])
+//             summed += sequence[++r]
+//             summed -= sequence[l++]
+//         }
+//     }
+//     // 후보 중 가장 차가 적은 것을 리턴
+//     return candidate.sort((a, b) => (a[1] - a[0]) - (b[1] - b[0]))[0] 
+// }
+
+function solution(seq, k) {
+    var answer = [], sum = 0, l = 0, r = 0, n = seq.length;
+    while(l <= r && r < n) {
+        sum += seq[r];
+        if (sum >= k) {
+            while(sum > k) {
+                sum -= seq[l];
+                l++;
+            }
+            if (sum === k) answer.push([l, r]);
         }
+        r++;
     }
-    // 후보 중 가장 차가 적은 것을 리턴
-    return candidate.sort((a, b) => (a[1] - a[0]) - (b[1] - b[0]))[0] 
+    return answer.sort((a, b) => {
+        const adis = a[1] - a[0];
+        const bdis = b[1] - b[0];
+        return adis === bdis ? a[0] - b[0] : (adis - bdis);
+    })[0];
 }
