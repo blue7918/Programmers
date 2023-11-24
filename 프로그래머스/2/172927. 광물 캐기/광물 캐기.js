1ee073c26153c3daf8cac5ddcb7e6169cfec54ef
blue7918 <115068278+blue7918@users.noreply.github.com>
2023-11-24 21:03:41 +0900

    [level 2] Title: 광물 캐기, Time: 313.76 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -0,0 +1,17 @@
+const calc = (minerals, pick) => {
+    const pirodo = {'diamond': [1, 5, 25], 'iron': [1, 1, 5], 'stone': [1, 1, 1]};
+    return minerals.reduce((p, c) => p + pirodo[c][pick], 0);
+}
+function solution(picks, minerals) {
+    let answer = 1 << 30;
+    if (picks.reduce((p, c) => p + c)) {
+        for(let i = 0; i < 3; i++) {
+            if (picks[i] > 0) {
+                let ps = [ ...picks ];
+                ps[i]--;
+                answer = Math.min(calc(minerals.slice(0, 5), i) + solution(ps, minerals.slice(5)), answer);
+            }
+        }
+        return answer;
+    } else return 0;
+}
