abda48bc4a237a6d4e00bc41fcb99eebc1da5160
blue7918 <115068278+blue7918@users.noreply.github.com>
2024-01-13 05:01:27 +0900

    [level Lv.3] Title: 아이템 줍기, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.3/87694. 아이템 줍기/아이템 줍기.js
+++ 프로그래머스/Lv.3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,44 +1,48 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
+    //각 좌표를 2배씩 해줌
     characterX *= 2;
     characterY *= 2;
     itemX *= 2;
     itemY *= 2;
-    let dobuleRec = rectangle.map(rec => rec.map(point => point * 2));
+    let doubleRec = rectangle.map((rec)=>(rec.map(point => point*2)));
     
     const moveX = [1, -1, 0, 0];
     const moveY = [0, 0, 1, -1];
     
-    const start = [characterX, characterY, 0];
+    const start = [characterX, characterY, 0]; //시작 위치
     let que = [start];
     
-    let range = Array.from({ length: 103 }, () => Array(103).fill(0));
- 
-    dobuleRec.forEach(([x1, y1, x2, y2]) => {
-        for (let i = x1; i <= x2; i++) {
-          for (let j = y1; j <= y2; j++) {
-            if (i === x1 || i === x2 || j === y1 || j === y2) {
-              if (range[i][j] === 0) range[i][j] = 1;
-            } else {
-              range[i][j] = 2;
+    let range = Array.from({ length: 103 }, () => Array(103).fill((0)))
+    
+    //테두리 체크
+    doubleRec.forEach(([x1, y1, x2, y2])=>{
+        for(let i = x1; i <= x2; i++){
+            for(let j = y1; j <= y2; j++){
+                if(i === x1 || i === x2 || j === y1 || j === y2){
+                    if(range[i][j] === 0) range[i][j] = 1;    
+                } else range[i][j] = 2;
+                
             }
-          }
         }
     });
-
-    range[characterX][characterY] = 0
     
+    range[characterX][characterY] = 0;
+    
+    //bfs
     while(que.length > 0){
-        let [x, y, cnt] = que.shift();
-        if(x === itemX && y === itemY) return cnt/2;
-
-        for(let i=0; i<4; i++){
-            let chX = x + moveX[i];
-            let chY = y + moveY[i];
-            if(range[chX][chY] === 1) {
-                que.push([chX, chY, cnt+1]); 
-                range[chX][chY] = 0;
+        let [x,y,move] = que.shift();
+        if(x === itemX && y === itemY) return move/2;
+        
+        for(let i = 0; i<4;i++){
+            let nx = x+moveX[i];
+            let ny = y+moveY[i];
+            if(range[nx][ny] === 1){
+                que.push([nx, ny, move+1]);
+                range[nx][ny] = 0;
             }
         }
     }
+    
     return 0;
+    
 }
